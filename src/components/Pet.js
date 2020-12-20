d102044fc69e7a405d873d21d9522f5fbd5bc06e
mtrimner <matt.trimner@gmail.com>
2020-12-20 14:13:12 -0800

    commit tree e40c77097ddd7820e1337f07a13fe33c8048334b

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,28 +1,55 @@
 import React from 'react'
 
 class Pet extends React.Component {
+  handleClick = () => {
+    this.props.onAdoptPet(this.props.pet.id)
+  }
   render() {
+    if (this.props.pet.isAdopted === false) {
     return (
       <div className="card">
         <div className="content">
           <a className="header">
             {/*'♀' OR '♂' */}
-            PET NAME
+            {this.props.pet.name}
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+          <button className="ui primary button" onClick={this.handleClick}>Adopt pet</button>
+        </div>
+      </div>
+    )
+    } else {
+    return (
+      <div className="card">
+        <div className="content">
+          <a className="header">
+            {/*'♀' OR '♂' */}
+            {this.props.pet.name}
+            {this.props.pet.gender === 'female' ? '♀' : '♂'}
+          </a>
+          <div className="meta">
+            <span className="date">{this.props.pet.type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
           </div>
         </div>
         <div className="extra content">
           <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
         </div>
       </div>
     )
+    }
   }
 }
 
